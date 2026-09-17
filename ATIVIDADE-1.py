2570dbf6c97df417fd09953c08f7bc6d84449b73
carvalho-oss <julia.k.rocha@ba.estudante.senai.br>
2026-09-17 16:50:26 -0300

    Add files via upload

---
+++ ATIVIDADE-1.py
@@ -0,0 +1,15 @@
+import os
+os.system ('cls')
+
+num1 = int(input( 'digite seu primeiro numero:'))
+num2 = int(input('digite seu segundo numero:'))
+
+if num1 == num2:
+    resultado = (f' número iguais: {num1 + num2}')
+
+else:
+    resulatado = (f' número diferentes: {num1 * num2}')
+
+    print (resultado)
+
+    print('=== fim do programa')
